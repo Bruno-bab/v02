1be1ba8626a2180b107362adb9a6443e31b59500
Bruno B. <brbaburi@vsite.hr>
2024-11-03 15:27:15 +0100

    v02 riješen

--- app/app.cpp
+++ app/app.cpp
@@ -1,5 +1,4 @@
 #include "app.h"
-#include <algorithm>
 
 namespace vsite::oop::v2
 {
@@ -7,5 +6,69 @@ namespace vsite::oop::v2
 * class member function implementations
 */
 
+    std::string to_hex(int value)
+    {
+        return std::format("{:X}", value);
+    }
+
+
+    color::color() : red(0.0), green(0.0), blue(0.0) {}
+
+    color::color(const color& other) : red(other.red), green(other.green), blue(other.blue) {}
+
+    void color::set_red(double r) 
+    {
+        if (r < 0)
+            red = 0.;
+        else if (r > 1)
+            red = 1.;
+        else
+            red = r;
+    }
+
+    void color::set_green(double g)
+    {
+        if (g < 0)
+            green = 0.;
+        else if (g > 1)
+            green = 1.;
+        else
+            green = g;
+    }
+
+    void color::set_blue(double b)
+    {
+        if (b < 0)
+            blue = 0.;
+        else if (b > 1)
+            blue = 1.;
+        else
+            blue = b;
+    }
+
+    double color::get_red() const { return red; }
+    double color::get_green() const { return green; }
+    double color::get_blue() const { return blue; }
+
+    color& color::operator=(const color& other)
+    {
+        if (this != &other)
+        {
+            red = other.red;
+            green = other.green;
+            blue = other.blue;
+        }
+        return *this;
+    }
+    
+    uint32_t color::get_color_ref() const
+    {
+        return RGB((int)(red * 255), (int)(green * 255), (int)(blue * 255));
+    }
+
+    double color::get_luminance() const
+    {
+        return (0.2126 * red + 0.7152 * green + 0.0722 * blue);
+    }
 
 } // namespace
--- app/app.h
+++ app/app.h
@@ -1,6 +1,6 @@
 #pragma once
+#include <format>
 
-#include <cstdint>
 // from <windows.h>
 #define RGB(r,g,b) ((uint32_t)(((uint8_t)(r)|((uint16_t)((uint8_t)(g))<<8))|(((uint32_t)(uint8_t)(b))<<16)))
 
@@ -9,6 +9,33 @@ namespace vsite::oop::v2
 /*
 * class declaration
 */
+    std::string to_hex(int value);
 
+
+    class color {
+    private:
+        double red;
+        double green;
+        double blue;
+
+    public:
+        color();
+
+        void set_red(double r);
+        void set_green(double g);
+        void set_blue(double b);
+
+        double get_red() const;
+        double get_green() const;
+        double get_blue() const;
+
+        color(const color& other);
+
+        color& operator=(const color& other);
+
+        uint32_t get_color_ref() const;
+
+        double get_luminance() const;
+    };
 
 }
